250bff5adab11e139e79c4085889aae5ec6c50b8
Madhura <madhurakshatriya333@gmail.com>
2021-03-16 15:27:41 +0530

    initial commit

---
+++ test.sql
@@ -0,0 +1,6 @@
+create table studentData (roll_no INT NOT NULL AUTO_INCREMENT PRIMARY KEY, sname TEXT, address TEXT, mobile_number TEXT, pan_number TEXT);
+insert into studentData (sname, address, mobile_number, pan_number) values ('Tom', 'Boston', '453622', 'ZXCVB1122Z');
+insert into studentData (sname, address, mobile_number, pan_number) values ('Bill', 'Mexico', '456213', 'ABCDE0012A');
+insert into studentData (sname, address, mobile_number, pan_number) values ('Lilly', 'Zurich', '478611', 'QWERT4567M');
+insert into studentData (sname, address, mobile_number, pan_number) values ('Amy', 'Sweden', '428764', 'ASDFG4321Q');
+insert into studentData (sname, address, mobile_number, pan_number) values ('Riya', 'India', '476551', 'LKJHG5643K');
