32f6cc6fe060b8d267195c5c779e3cb5b2c0667e
madhurak3 <80592396+madhurak3@users.noreply.github.com>
2021-03-30 21:49:38 +0530

    Add files via upload

---
+++ Assignment2/test2.sql
@@ -0,0 +1,6 @@
+create table icecreamData (lot_no INT NOT NULL AUTO_INCREMENT PRIMARY KEY, iname TEXT, price TEXT, quantity INT);
+insert into icecreamData (iname, price, quantity) values ('Fruit_Overload', '120', '7');
+insert into icecreamData (iname, price, quantity) values ('ButterScotch', '110', '12');
+insert into icecreamData (iname, price, quantity) values ('CookiesAndCream', '130', '8');
+insert into icecreamData (iname, price, quantity) values ('PineappleSupreme', '120', '3');
+insert into icecreamData (iname, price, quantity) values ('SwissCake', '150', '5');
---
+++ Assignment2/test3.sql
@@ -0,0 +1,6 @@
+create table aircraftData (aid INT NOT NULL AUTO_INCREMENT PRIMARY KEY, aname TEXT, origin TEXT, quantity INT);
+insert into aircraftData (aname, origin, quantity) values ('Sukhoi', 'Russia_India', '300');
+insert into aircraftData (aname, origin, quantity) values ('Miraj', 'France', '500');
+insert into aircraftData (aname, origin, quantity) values ('Tejas', 'India', '1200');
+insert into aircraftData (aname, origin, quantity) values ('Jaguar', 'France_UK', '905');
+insert into aircraftData (aname, origin, quantity) values ('Saras', 'India', '50');
